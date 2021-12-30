973284468f63d4d74dd6ca28105a50722b641088
Fortuna-w <wh13636071306@163.com>
2021-12-30 17:55:57 +0800

    12/30--数组和堆

---
+++ datastructure/datastructure/main.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+void test1() {
+	// 初始化小顶堆
+	priority_queue<int, vector<int>, greater<int>> heap;
+
+	// 元素入堆
+	heap.push(1);
+	heap.push(10);
+	heap.push(2);
+	heap.push(6);
+	heap.push(8);
+
+	// 元素出堆（从小到大）
+	for (int i = 0; i < 5; i++) {
+		cout << heap.top() << endl;
+		heap.pop();
+	}
+}
+
+void test2() {
+	//数组
+	int a[5];
+	int n;
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%d", &n);
+		a[i] = n;
+	}
+	for (int i = 0; i < 5; i++) {
+		cout << "a["<<i<<"]="<< a[i];
+		cout << endl;
+		//printf("a[%d]=%d\n", i, a[i]);
+	}
+	vector<int> arr;
+	arr.push_back(1);
+	arr.push_back(4);
+	arr.push_back(9);
+	arr.push_back(16);
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << endl;
+	}
+	vector<int>::iterator t;
+	for (t = arr.begin(); t != arr.end(); t++) {
+		cout << *t << endl;
+	}
+
+}
+int main() {
+	
+
+	test1();
+
+	test2();
+
+	return 0;
+}
+
